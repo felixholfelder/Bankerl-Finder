5982ab8cd48e09354c8e7fe8d15d4293500e64ec
Felix Holfelder <felixholfelder1@t-online.de>
2024-09-27 12:51:52 +0200

    first tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 
 void main() {
@@ -23,11 +24,25 @@ class MyApp extends StatelessWidget {
             FlutterMap(
               options: const MapOptions(
                 initialZoom: 10,
+                initialCenter: LatLng(49.9012148, 12.2009079)
               ),
               children: [
                 TileLayer(
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                   userAgentPackageName: 'com.example.app',
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: LatLng(49.9012148, 12.2009079),
+                      child: const Icon(
+                        Icons.location_on,
+                        color: Colors.red,
+                        size: 30,
+                      ),
+                      rotate: true,
+                    ),
+                  ],
                 ),
               ],
             ),
