7996535569f2712d3847630a3c9e9ff33e82ba0c
Felix Holfelder <felixholfelder1@t-online.de>
2024-10-04 13:06:34 +0200

    added maps_launcher

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:bankerl_finder/model/Bench.dart';
 import 'package:bankerl_finder/service/BenchService.dart';
 import 'package:bankerl_finder/service/DialogService.dart';
@@ -6,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,6 +28,10 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     _goToCurrPosition();
     _getBenches();
+
+    print(_center.toJson());
+
+    _benches.add(_getMarker(LatLng(49.9012148, 12.2009079)));
   }
 
   @override
@@ -33,10 +40,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Bankerl Finder"),
-        actions: [
-          IconButton(
-              onPressed: _goToCurrPosition, icon: const Icon(Icons.my_location))
-        ],
+        actions: [IconButton(onPressed: _goToCurrPosition, icon: const Icon(Icons.my_location))],
       ),
       body: Stack(
         children: [
@@ -44,12 +48,11 @@ class _HomePageState extends State<HomePage> {
             options: MapOptions(
                 initialZoom: 14,
                 initialCenter: _center,
-                onTap: (TapPosition pos, LatLng latlng) =>
-                    _openBenchDialog(context, pos, latlng)),
+                onTap: (TapPosition pos, LatLng latlng) => _openBenchDialog(context, pos, latlng),
+                interactionOptions: const InteractionOptions(flags: InteractiveFlag.drag | InteractiveFlag.pinchZoom),
+                maxZoom: 20),
             children: [
-              TileLayer(
-                  urlTemplate:
-                      'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
+              TileLayer(urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
               MarkerLayer(
                 markers: [
                   Marker(
@@ -57,7 +60,7 @@ class _HomePageState extends State<HomePage> {
                     child: const Icon(
                       Icons.location_on,
                       color: Colors.red,
-                      size: 30,
+                      size: 40,
                     ),
                     rotate: true,
                   ),
@@ -80,17 +83,14 @@ class _HomePageState extends State<HomePage> {
             title: const Text("Bank anlegen"),
             actionsOverflowButtonSpacing: 20,
             actions: [
-              TextButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: const Text("Abbrechen")),
+              TextButton(onPressed: () => Navigator.of(context).pop(), child: const Text("Abbrechen")),
               ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).pop();
                   _addBench(latlng);
                 },
                 style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    foregroundColor: Colors.white),
+                    backgroundColor: Theme.of(context).primaryColor, foregroundColor: Colors.white),
                 child: const Text("Speichern"),
               ),
             ],
@@ -104,19 +104,43 @@ class _HomePageState extends State<HomePage> {
     List<Bench> benches = await _benchService.getBenches(context);
 
     _benches.addAll(
-      benches.map(
-        (e) => Marker(
-          point: LatLng(e.position.latitude, e.position.longitude),
-          child: const Icon(
-            Icons.location_on,
-            color: Colors.blue,
-            size: 30,
-          ),
-          rotate: true,
-        ),
+      benches.map((e) => _getMarker(e.position)),
+    );
+  }
+
+  Marker _getMarker(LatLng position) {
+    return Marker(
+      point: position,
+      child: GestureDetector(
+        onTap: () => _showMarkerBottomSheet(position),
+        child: const Icon(Icons.location_on, color: Colors.blue, size: 40),
       ),
     );
   }
+
+  void _showMarkerBottomSheet(LatLng position) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+            height: 100,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(children: [
+                Text("${position.latitude}, ${position.longitude}", style: const TextStyle(fontSize: 20)),
+                const Spacer(),
+                IconButton(
+                  tooltip: "In Google Maps öffnen",
+                  onPressed: () => _openGoogleMaps(position),
+                  icon: const Icon(Icons.near_me, size: 28),
+                ),
+              ]),
+            ));
+      },
+    );
+  }
+
+  void _openGoogleMaps(LatLng position) => MapsLauncher.launchCoordinates(position.latitude, position.longitude);
 
   void _addBench(LatLng pos) {
     Bench bench = Bench(name: "", position: pos);
@@ -134,12 +158,9 @@ class _HomePageState extends State<HomePage> {
 
     if (!hasPermission)
       SnackBarService.errorSnackBar(
-          context: context,
-          title: "GPS-Fehler",
-          message: "GPS ist für diese App deaktiviert!");
+          context: context, title: "GPS-Fehler", message: "GPS ist für diese App deaktiviert!");
 
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     return position;
   }
 
@@ -149,10 +170,7 @@ class _HomePageState extends State<HomePage> {
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      SnackBarService.warningSnackBar(
-          context: context,
-          title: "GPS-Fehler",
-          message: "Dein GPS ist ausgeschaltet!");
+      SnackBarService.warningSnackBar(context: context, title: "GPS-Fehler", message: "Dein GPS ist ausgeschaltet!");
       return false;
     }
     permission = await Geolocator.checkPermission();
@@ -160,9 +178,7 @@ class _HomePageState extends State<HomePage> {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
         SnackBarService.warningSnackBar(
-            context: context,
-            title: "GPS-Fehler",
-            message: "GPS ist für diese App deaktiviert!");
+            context: context, title: "GPS-Fehler", message: "GPS ist für diese App deaktiviert!");
         return false;
       }
     }
@@ -170,8 +186,7 @@ class _HomePageState extends State<HomePage> {
       SnackBarService.warningSnackBar(
           context: context,
           title: "GPS-Fehler",
-          message:
-              "GPS ist für diese App deaktiviert!. Ändere deine Einstellungen um die GPS-Suche zu verwenden!");
+          message: "GPS ist für diese App deaktiviert!. Ändere deine Einstellungen um die GPS-Suche zu verwenden!");
       return false;
     }
     return true;
